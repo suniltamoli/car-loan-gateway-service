7dbf5ab0e0c51b9d7993616638734b9b0abcfa05
sunilkumartamoli <suniltamoli@gmail.com>
2020-10-25 17:22:17 +0530

    initial commit

---
+++ src/main/java/com/sg/loan/gateway/APIGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sg.loan.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APIGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APIGatewayApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sg/loan/gateway/config/SpringCloudConfig.java
@@ -0,0 +1,24 @@
+package com.sg.loan.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/kyc-service/v1/carloan/customer/kyc/**")
+                        .uri("http://localhost:8081/")
+                        .id("kyc-service"))
+
+                .route(r -> r.path("/car-loan-service/v1/carloan/**")
+                        .uri("http://localhost:8082/")
+                        .id("consumerModule"))
+                .build();
+    }
+
+}
